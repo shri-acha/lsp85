5bf9f2c76e403537b6ebb6cbc16da21a75cebb3b
shri-acha <happy.irhs@gmail.com>
2026-04-24 09:08:52 +0545

    refactor(server): hover handler

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -1,16 +1,12 @@
-use core::fmt;
-
-use crate::server::completion_items::get_completion_items;
+use crate::frontend::token::TokenType;
+use crate::{frontend::lexer::Lexer, server::completion_items::get_completion_items};
 use crate::server::utils::get_documentation;
 use lsp_server::{RequestId};
 use lsp_types::{
     CompletionParams, CompletionResponse, HoverParams,
 };
 
-use crate::frontend::lexer::Lexer;
-use crate::frontend::parser::{Parser,Node};
-use crate::frontend::token::{Token, TokenType,Location};
-use crate::frontend::utils::files::{get_source_buffer,get_source_line};
+use crate::frontend::utils::files::get_source_line;
 
 pub fn completion_handler(
     id: &RequestId,
@@ -33,11 +29,17 @@ pub fn hover_handler(
     let hovered_word = get_source_line(file_name, position.line)
         .and_then(|source| source.ok())
         .and_then(|line| {
-            let col = position.character as usize;
-            let start = line[..col].rfind(|c: char| !c.is_alphanumeric()).map(|i| i + 1).unwrap_or(0);
-            let end = line[col..].find(|c: char| !c.is_alphanumeric()).map(|i| i + col).unwrap_or(line.len());
-            Some(line[start..end].trim().to_uppercase())
-        });
+            let col = position.character as usize; 
+            let lexer = Lexer::new(line,position.line as usize);
+            lexer
+                .filter(|tok| {matches!(tok.tok_type, TokenType::OPERATION | TokenType::REGISTER | TokenType::IMM_VALUE)})
+                .find(|tok| {
+                        let tok_start = tok.location.col - tok.offset;
+                        let tok_end = tok.location.col;
+                        (col >= tok_start) && (col < tok_end)
+                    })
+        })
+        .map(|token| token.tok_literal);
 
     let info = hovered_word.and_then(|word| {
         get_documentation()
